848db3e43e265f3924feea1cd5d86fbcffec9a8e
dzino <mak-okunev@yandex.ru>
2021-10-02 17:56:53 +0500

    PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const withPWA = require("next-pwa")
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+})
